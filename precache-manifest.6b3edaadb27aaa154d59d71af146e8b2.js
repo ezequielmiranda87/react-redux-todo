f7f47f60e2e8561b55c8796033a68e4c3cad4eb5
ezex87 <ezequielmiranda87@gmail.com>
2019-02-22 13:32:09 +0100

    Updates

---
+++ precache-manifest.6b3edaadb27aaa154d59d71af146e8b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ba569572ef2fb572b5",
+    "url": "/react-redux-todo/static/js/runtime~main.24ba5695.js"
+  },
+  {
+    "revision": "2aba6f18aafa70584d75",
+    "url": "/react-redux-todo/static/js/main.2aba6f18.chunk.js"
+  },
+  {
+    "revision": "a75237ea0ef595a14061",
+    "url": "/react-redux-todo/static/js/2.a75237ea.chunk.js"
+  },
+  {
+    "revision": "2aba6f18aafa70584d75",
+    "url": "/react-redux-todo/static/css/main.d47bae35.chunk.css"
+  },
+  {
+    "revision": "3474cf17578701ee003b804b533cf2d8",
+    "url": "/react-redux-todo/index.html"
+  }
+];
